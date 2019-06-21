dd8a6073b73d7f5fc5360ba0608c314f4e662c37
Harry Nguyen <sonhanguyen@outlook.com>
2019-06-21 20:55:08 +1000

    Minor refactoring

--- index.ts
+++ index.ts
@@ -1,10 +1,139 @@
 import * as React from 'react'
 
-type Props<C> = C extends React.ComponentType<infer P> ? P : {}
+type Adapter<T, HookProps extends {} = {}> = <Map extends MapProps<T> = MapProps<T, T>>(_?: Map) =>
+  <P extends Injected, Injected = ReturnType<Map>>(_: React.ComponentType<P>) => React.ComponentType<
+    & HookProps
+    & Options<Map>
+    & Partial<Injected>
+    & Pick<P, Exclude<keyof P, keyof Injected>>
+  >
+
+const createAdapter = <T, P = {}>(hook: (_?: P) => T): Adapter<T, P> =>
+  (map = (_ => _) as any) => component => {
+    const memoised = React.memo(component)
+
+    return (props: P) => React.createElement(
+      memoised as any,
+      { ...map(hook(props), props), ...props }
+    )
+  }
+
+type MapProps<From = any, To = any, Props extends {} = {}> = (_: From, options?: Props) => To
+type Options<A extends MapProps> = A extends MapProps<any, any, infer P> ? P : {}
+
+
+type Mutations<T> = {
+  [K in keyof T]: T[K] extends (..._) => any
+    ? (..._: Parameters<T[K]>) => void
+    : () => void
+}
+
+const createMutations = <T, M extends Updates<T>>(
+  updates: M,
+  dispatch: (_ :Update<T>) => void
+) => {
+  const mutations: Partial<Mutations<M>> = {}
+  for (const action in updates) {
+    const update = updates[action]
+    mutations[action] = ((...args) => dispatch(
+      typeof update === 'function'
+        ? update(...args)
+        : update
+    )) as any
+  }
+
+  return mutations as Mutations<M>
+}
+
+export type ProviderModule<T, P = {}> = {
+  (): T // hook
+  Adapter: Adapter<T>
+  Provider: React.ForwardRefExoticComponent<ProviderProps<T, P>>
+}
+
+type ProviderProps<T, P> = P & {
+  ref?: React.Ref<T>
+  children: React.ReactNode
+}
+
+const useUnboundRef = <T>(value, ref?: React.Ref<T>) => {
+  if (!ref) return
+
+  let functionRef = ref as (_?: T) => void
+  if (typeof ref !== 'function') {
+    const mutableRef = ref as React.MutableRefObject<T>
+    functionRef = (value: T = null) => mutableRef.current = value
+  }
+
+  React.useEffect(() => {
+    functionRef(value)
+    return () => functionRef(null)
+  }, [])
+}
+
+export const createStore = <
+  State,
+  UpdateFactories extends Updates<State>,
+  Props extends {} = {},
+>(
+  factory: (_?: Props) => State,
+  updates: UpdateFactories = {} as any
+) => (initial: Props): State & Mutations<UpdateFactories> => {
+  const make = () => {
+    const model = factory(initial)
+
+    return Object.assign(model, createMutations(
+      updates, update => dispatch(update) // dispatch to be lazily dereferenced
+    ))
+  }
+
+  const [ value, dispatch ] = React.useReducer(
+    reducer,
+    React.useMemo(make, [])
+  )
+
+  return value
+}
+
+const createHook = <
+  State,
+  UpdateFactories extends Updates<State>,
+  Props extends {} = {}
+>(
+  initial: (_?: Props) => State,
+  updates: UpdateFactories = {} as any
+): ProviderModule<State & Mutations<UpdateFactories>, Props> => {
+  const context = React.createContext({})
+  const hook = () => React.useContext(context)
+  const Adapter = createAdapter(hook)
+  const useStore = createStore(initial, updates)
+
+  type Service = ReturnType<typeof useStore>
+
+  const Provider = React.forwardRef<Service, ProviderProps<Props, Service>>((props, ref) => {
+    const { children, ...providerProps } = props
+
+    const value = useStore(providerProps as any)
+    useUnboundRef(value, ref)
+
+    return React.createElement(context.Provider, { children, value })
+  })
+
+  return Object.assign(hook, { Provider, Adapter })
+}
+
+export default createHook
+
+export type Service<T extends ProviderModule<any>> = ReturnType<T>
+
 // for ergonomic reason, T should not be of function type
-export type Update<T> =
-  | { (_: T): Partial<T> }
-  | Partial<T>
+export type Update<T> = { (_: T): void | Partial<T> }
+
+
+type Updates<T> = Record<string,
+| ((..._) => Update<T> )
+| Partial<T>
+>
 
 const reducer = <T>(state: T, update: Update<T>): T => {
   const nextState = typeof update == 'function'
@@ -16,50 +145,5 @@ const reducer = <T>(state: T, update: Update<T>): T => {
     : state
 }
 
-const createConnect = <T>(hook: () => T): ProviderModule<T>['connect'] =>
-  (map = (_ => _) as any) => component => {
-    const memoised = React.memo(component)
-
-    return props => React.createElement(
-      memoised as any,
-      { ...props, ...map(hook(), props) }
-    )
-  }
-
-type Adapter<T, P, V> = (context: T, props: P) => V
-
-export type ProviderModule<T, P = {}> = {
-  (): T // hook
-  Provider: React.ComponentType<P & { children: React.ReactNode }>
-  connect<V, O = {}>(map?: Adapter<T, O, V>):
-    <Props extends O>(_: React.ComponentType<Props>) => React.ComponentType<Props & V>
-}
-
-const createHook = <T, P = {}>(
-  factory: (getNextToMerge: (_: Update<T>) => void, providerProps: P) => T
-): ProviderModule<T, P> => {
-  const context = React.createContext<T>({} as any)
-  const hook = () => React.useContext(context)
-  const connect: any = createConnect(hook)
-
-  const Provider = (props: Props<ProviderModule<T, P>['Provider']>) => {
-    const { children, ...providerProps } = props
-
-    const [ value, dispatch ] = React.useReducer(
-      reducer,
-      React.useMemo(
-        () => factory(
-          update => dispatch(update),
-          providerProps as any,
-        ),
-        []
-      )
-    )
-
-    return React.createElement(context.Provider, { children, value })
-  }
-
-  return Object.assign(hook, { Provider, connect })
-}
-
-export default createHook
+export const mapProps = <T, P>(hook: (_: P) => T) => createAdapter(hook)()
+// shorthand
--- spec.tsx
+++ spec.tsx
@@ -1,63 +1,205 @@
 import * as React from 'react'
-import createHook, { Update } from '.'
+import createHook, { Service } from '.'
 import { render } from '@testing-library/react'
+import assert from 'assert'
 
-type State = Record<'on' | 'off', { (): void }>
+const factory = jest.fn((props: { on?: true }) => ({
+  isOn: !!props.on,
+  isDisabled: false
+}))
 
-const mockFactory = (
-  mergeState: (_: Update<State>) => void,
-  initially: { on?: true }
-): Partial<State> => {
-  const on = () => mergeState({ on: undefined, off })
-  const off = () => mergeState({ off: undefined, on })
-
-  return initially.on ? { off } : { on }
+const actions = {
+  on: { isOn: true },
+  off: () => () => ({ isOn: false }),
+  disable: (isOn: boolean = false) => () => ({ isOn, isDisabled: true })
 }
 
-const useService = createHook(
-  mockFactory
+const useService = createHook(factory, actions)
+
+const Test = jest.fn((viewModel: Service<typeof useService>) =>
+  <>{ Object
+      .keys(actions)
+      .map(key =>
+        <button id={key} key={key} onClick={() => viewModel[key]()} />
+      )
+    }
+  </>
 )
 
-const Test = () => {
-  const service = useService()
-  const next = (['on', 'off'] as const).find(it => service[it])
-
-  return <button onClick={service[next]}>{next}</button>
+const lastProps = ({ mock }: jest.Mock) => {
+  const [[ props ]] = mock.calls
+  return props
 }
+
+const TestWithIdentityAdapter = useService.Adapter()(Test)
 
 describe('Stateless', () => {
   it('Should render', () => {
-    expect(
-      render(
-        <useService.Provider>
-          <Test />
-        </useService.Provider>
-      ).container.textContent
-    ).toBe('on')
+    Test.mockClear()
+    render(
+      <useService.Provider>
+        <TestWithIdentityAdapter />
+      </useService.Provider>
+    )
+
+    let props = lastProps(Test)
+    expect(props).toMatchObject({ isDisabled: false, isOn: false })
+
+    Test.mockClear()
+    render(
+      <useService.Provider on>
+        <TestWithIdentityAdapter />
+      </useService.Provider>
+    )
+
+    props = lastProps(Test)
+    assert(props.isOn === true, 'render with provider props fails')
   })
 
-  it('Should render with provider props', () => {
-    expect(
-      render(
-        <useService.Provider on>
-          <Test />
-        </useService.Provider>
-      ).container.textContent
-    ).toBe('off')
+  it('Should provide stateless service object if actions are not provided', () => {
+    const useStatelessService = createHook(factory)
+    const TestStatelessService = useStatelessService.Adapter()(Test)
+
+    Test.mockClear()
+    render(
+      <useStatelessService.Provider on>
+        <TestStatelessService />
+      </useStatelessService.Provider>
+    )
+
+    const props = lastProps(Test)
+    expect(props).toMatchObject({ isDisabled: false, isOn: true })
   })
 })
 
 describe('Stateful', () => {
   it('Should rerender', () => {
+    Test.mockClear()
     const { container } = render(
       <useService.Provider>
-        <Test />
+        <TestWithIdentityAdapter />
       </useService.Provider>
     )
 
-    const [button] = container.getElementsByTagName('button')
-    expect(button.textContent).toBe('on')
+    let button = container.querySelector<HTMLButtonElement>('#on')
+    let props = lastProps(Test)
+    expect(props).toMatchObject({ isDisabled: false, isOn: false })
+
+    Test.mockClear()
     button.click()
-    expect(button.textContent).toBe('off')
+    props = lastProps(Test)
+
+    assert(
+      props.isOn === true,
+      'fails to account for constant state mutation (Partial<State>)'
+    )
+
+    button = container.querySelector<HTMLButtonElement>('#off')
+    Test.mockClear()
+    button.click()
+    props = lastProps(Test)
+
+    assert(
+      props.isOn === false,
+      'fails to account for mutaion that is function of previous state (() => (_: State) => Partial<State>)'
+    )
+
+    button = container.querySelector<HTMLButtonElement>('#disable')
+    Test.mockClear()
+    button.click()
+    const { isOn, isDisabled } = lastProps(Test)
+
+    assert(
+      (isOn === false) && (isDisabled === true),
+      'fails to account for mutaion that takes params ((..._) => (_: State) => Partial<State>)'
+    )
+  })
+})
+
+describe('hoc', () => {
+  it('Should not rerender if viewModel is unchanged', () => {
+    const { container } = render(
+      <useService.Provider>
+        <TestWithIdentityAdapter />
+      </useService.Provider>
+    )
+
+    const button = container.querySelector<HTMLButtonElement>('#on')
+    Test.mockClear()
+    button.click()
+    expect(Test).toBeCalledTimes(1)
+
+    Test.mockClear()
+    button.click()
+    expect(Test).not.toBeCalled()
+  })
+
+  it('Should work with a custom mapProps function', () => {
+    const ConsumeNamespaced = <NS extends string>(props:
+      & Record<NS, Service<typeof useService>>
+      & { namespace: NS }
+      // @ts-ignore
+    ) => <Test { ...props[props.namespace] } />
+
+    const TestNamespaceAdapter = useService.Adapter(
+      <NS extends string>(viewModel, props: { namespace: NS }) => ({ [props.namespace]: viewModel })
+    )(ConsumeNamespaced)
+
+    Test.mockClear()
+    render(
+      <useService.Provider>
+        <TestNamespaceAdapter namespace='viewModel' />
+      </useService.Provider>
+    )
+
+    expect(lastProps(Test)).toMatchObject({ isDisabled: false, isOn: false })
+  })
+})
+
+describe('Provider', () => {
+  type Instance = Service<typeof useService>
+
+  describe('should foward Service instance', () => {
+    const testOnMount = (ref: Instance) => {
+      expect(
+        Object.keys(ref).sort()
+      ).toEqual(
+        [ 'on', 'off', 'disable', 'isOn', 'isDisabled' ].sort()
+      )
+    }
+
+    const testOnUnmount = ref => {
+      assert(ref === null, 'should forward null on unmount')
+    }
+
+    it('via ref object', () => {
+      const ref = React.createRef<Instance>()
+      const { unmount } = render(<useService.Provider ref={ref} children={null} />)
+
+      testOnMount(ref.current)
+      unmount()
+      testOnUnmount(ref.current)
+    })
+
+    it('via function ref', () => {
+      const ref = jest.fn(testOnMount)
+      const { unmount } = render(<useService.Provider ref={ref} children={null} />)
+
+      expect(ref).toBeCalledTimes(1)
+      ref.mockClear()
+      ref.mockImplementationOnce(testOnUnmount)
+      unmount()
+      expect(ref).toBeCalledTimes(1)
+    })
+  })
+
+  it('should NOT recreate instance when Provider\'s props are updated', () => {
+    factory.mockClear()
+    const { rerender } = render(<useService.Provider on children={null} />)
+    expect(factory).toBeCalledWith({ on: true })
+
+    factory.mockClear()
+    rerender(<useService.Provider children={null} />)
+    expect(factory).not.toBeCalled()
   })
 })
